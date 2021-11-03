d33b9c7b5ae9af58fc8681c041d4cadd099d1545
Gustavo Romero Cañas <romeroc.gus@gmail.com>
2021-11-03 03:23:01 +0100

    added active link class on navbar

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -3,15 +3,15 @@ import './Footer.css';
 
 function Footer() {
     return(
-        <footer id="footer" class="flex-footer">
-            <div class="footer__links">
-                <a class="footer__link" href="https://www.linkedin.com/in/gustavo-ezequiel-romero-ab0b171b2/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin"></i></a>
-                <a class="footer__link" href="https://www.instagram.com/romeroc.gus/" target="_blank" rel="noreferrer"><i class="fab fa-instagram"></i></a>
-                <a class="footer__link" href="https://www.facebook.com/gus.romeroc/" target="_blank" rel="noreferrer"><i class="fab fa-facebook-square"></i></a>
-                <a class="footer__link" href="https://github.com/romerocgus" target="_blank" rel="noreferrer"><i class="fab fa-github-square"></i></a>
+        <footer id="footer" className="flex-footer">
+            <div className="footer__links">
+                <a className="footer__link" href="https://www.linkedin.com/in/gustavo-ezequiel-romero-ab0b171b2/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a>
+                <a className="footer__link" href="https://www.instagram.com/romeroc.gus/" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a>
+                <a className="footer__link" href="https://www.facebook.com/gus.romeroc/" target="_blank" rel="noreferrer"><i className="fab fa-facebook-square"></i></a>
+                <a className="footer__link" href="https://github.com/romerocgus" target="_blank" rel="noreferrer"><i className="fab fa-github-square"></i></a>
             </div>
-		    <p class="footer__text">Gustavo Ezequiel Romero Cañas 
-                <span class="footer__copyright">© 2020</span>
+		    <p className="footer__text">Gustavo Ezequiel Romero Cañas 
+                <span className="footer__copyright">© 2020</span>
             </p>
 	</footer>
     );
--- src/Navbar/index.js
+++ src/Navbar/index.js
@@ -1,24 +1,49 @@
 import React from "react";
 import './Navbar.css';
 
+const navItemsArr = [
+	{
+		title: "Home",
+		url: "/",
+	},
+	{
+		title: "About",
+		url: "#About",
+	},
+	{
+		title: "Projects",
+		url:"#Projects",
+	},
+	{
+		title: "Contact",
+		url: "#Contact",
+	},
+];
+
 function Navbar(){
 	const [visible, setVisible] = React.useState(false);
+	const [activeLink, setActiveLink] = React.useState("");
 
 	const toggleList = ()=>{
 		setVisible(!visible);
+	};
+
+	const handlerActive = (text)=>{
+		setActiveLink(text);
 	}
 
     return(
         <nav className="navbar">
-		<div className={`navbar__button ${visible && 'cross'}`} onClick={toggleList}>
-		</div>
-		<ul className={`navbar__list-items ${visible && 'list-active'}`}>
-			<li className="navbar__item "><a href="index.html" className="navbar__link">Home</a></li>
-			<li className="navbar__item"><a href="#About" className="navbar__link">About</a></li>
-			<li className="navbar__item"><a href="#Projects" className="navbar__link">Projects</a></li>
-			<li className="navbar__item"><a href="#Contact" className="navbar__link">Contact</a></li>
-		</ul>
-	</nav>
+			<div className={`navbar__button ${visible ? 'cross :' : ""}`} onClick={toggleList}>
+			</div>
+			<ul className={`navbar__list-items ${visible ? 'list-active': ""}`}>
+
+				{navItemsArr.map(item => {
+					return (
+					<li key={item.title} className={`navbar__item ${activeLink === item.title ? "active" : ""}`}onClick={()=>handlerActive(item.title)}><a href={item.url} className="navbar__link">{item.title}</a></li>
+				)})}
+			</ul>
+		</nav>
     )
 }
 
